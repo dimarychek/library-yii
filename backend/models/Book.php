f562e8e4fd6c768e049eb0d80daaae65cf55771b
Dmitriy Rychek <d.rychek@labtop.digital>
2018-05-23 02:25:15 +0300

    create models & crud for author & book

---
+++ backend/models/Book.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace backend\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "book".
+ *
+ * @property int $id
+ * @property int $author_id
+ * @property string $name
+ */
+class Book extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'book';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['author_id', 'name'], 'required'],
+            [['author_id'], 'integer'],
+            [['name'], 'string', 'max' => 255],
+            [['name'], 'unique'],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'author_id' => 'Author ID',
+            'name' => 'Name',
+        ];
+    }
+}
